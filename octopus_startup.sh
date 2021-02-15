2762afa4d6f3892b323c95ab6156bfb64b968a6e
AntonCarter <acnet@me.com>
2021-02-15 21:42:55 +0000

    Update octopus_startup.sh

--- octopus_startup.sh
+++ octopus_startup.sh
@@ -2,20 +2,17 @@ cmd="up"
 octopus=0
 project_name="Merit"
 deploy_environment="dev"
-while getopts ":n:c:o:e" opt; do
+while getopts ":n:c:o:e:" opt; do
 	case $opt in
-		o) octopus=1
-		;;
-		n) project_name="$OPTARG"
-		;;
-		c) cmd="$OPTARG"
-		;;
-		e) deploy_environment="$OPTARG"
-		;;
-		\?) echo "Invalid option -$OPTARG" >&2
-		;;
+		o) octopus=1 ;;
+		n) project_name="$OPTARG" ;;
+		c) cmd="$OPTARG" ;;
+		e) deploy_environment="$OPTARG" ;;
+		\?) echo "Invalid option -$OPTARG" >&2 ;;
 	esac
 done
+
+echo Deploy Environment is "$deploy_environment"
 
 if [[ $cmd = "up" ]]; then
 	if [[ octopus -eq 1 ]]; then
